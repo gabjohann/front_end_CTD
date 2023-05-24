60bd28e443b06ba1ac26dff24c6a96004d146a84
Lucas Gabriel <gabif.johann@gmail.com>
2023-05-23 22:28:04 -0300

    aula 07

---
+++ aula07/src/App.jsx
@@ -0,0 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Form from './components/form';
+import Table from './components/table';
+
+const client = new QueryClient();
+
+export function App() {
+  return (
+    <QueryClientProvider client={client}>
+      <h1>Diário Eletrônico</h1>
+
+      <Form />
+
+      <Table />
+    </QueryClientProvider>
+  );
+}
